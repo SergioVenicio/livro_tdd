fdcaf18f686a9ebbdb73dfe27ecf433b10756fe2
sergio <sergionirvana2010@hotmail.com>
2018-07-06 18:14:21 -0300

    Inicio de 2º capitulo

--- functonal_test.py
+++ functonal_test.py
@@ -1,8 +1,26 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
-browser_path = FirefoxBinary('/opt/firefox/firefox')
-browser = webdriver.Firefox(firefox_binary=browser_path)
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self._path = FirefoxBinary('/opt/firefox/firefox')
+        self.browser = webdriver.Firefox(firefox_binary=self._path)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_letter(self):
+        # Edith ouviu falar de uma nova aplicação online interessante para
+        # lista de tarefas. Ela decide verificar sua homepage
+        self.browser.get('http://localhost:8000')
+
+        # Ela percebe que o titulo da página e o cabeçalho mencionam listas de
+        # taferefas (to-do)
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test')
+
+
+if __name__ == '__main__':
+    unittest.main()
