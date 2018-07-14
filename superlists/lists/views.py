432e24b06b704e4034143c86dd2241ddcbc826bd
sergio <sergionirvana2010@hotmail.com>
2018-07-14 11:58:30 -0300

    Capitulo 7

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -3,11 +3,8 @@ from django.shortcuts import render, redirect
 
 
 def home_page(request):
-    if request.method == 'POST':
-        new_item_text = request.POST.get('item_text', '')
-        models.Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-only-list-in-the-world/')
-
+    new_item_text = request.POST.get('item_text', '')
+    models.Item.objects.create(text=new_item_text)
     return render(request, 'home.html')
 
 
@@ -16,3 +13,8 @@ def view_list(request):
         'items': models.Item.objects.all()
     }
     return render(request, 'list.html', context)
+
+
+def new_list(request):
+    models.Item.objects.create(text=request.POST.get('item_text'))
+    return redirect('/lists/the-only-list-in-the-world/')
