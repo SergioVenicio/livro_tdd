412bae1e02ac73b030dd7f3598e953246c55f79d
sergio <sergionirvana2010@hotmail.com>
2018-07-14 11:41:21 -0300

    Capitulo 7

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -6,8 +6,13 @@ def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST.get('item_text', '')
         models.Item.objects.create(text=new_item_text)
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
+
+    return render(request, 'home.html')
+
+
+def view_list(request):
     context = {
         'items': models.Item.objects.all()
     }
-    return render(request, 'home.html', context)
+    return render(request, 'list.html', context)
