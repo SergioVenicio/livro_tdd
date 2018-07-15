31c1e0d0e1e157a15fb86511569a4ca88cab104b
sergio <sergionirvana2010@hotmail.com>
2018-07-15 18:30:43 -0300

    Fim do cápitulo 8

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,15 +1,15 @@
 import time
 from selenium import webdriver
-from django.test import LiveServerTestCase
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self._path = FirefoxBinary('/opt/firefox/firefox')
         self.browser = webdriver.Firefox(firefox_binary=self._path)
@@ -101,3 +101,24 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfeitos, ambos voltam a dormir
+
+    def test_layout_and_styling(self):
+        # Edith acessa a página inicial
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Ela percebe que a caixa de entrada está elegantemente centralizada
+        input_box = self.browser.find_element_by_id('id_new_item')
+
+        # Ela inicia uma nova lista e vê que a entrada está elegantemente
+        # centralizada aí também
+        input_box.send_keys('testing')
+        input_box.send_keys(Keys.ENTER)
+
+        self.wait_for_row_in_list_table('1: testing')
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512, delta=100
+        )
